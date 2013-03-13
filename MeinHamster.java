b842b96b51da44bd028e8cf32926a5fe3f59477e
JanUlrich <janulrich@anonymous.(none)>
2013-03-13 11:45:26 +0100

    Karte added

--- MeinHamster.java
+++ MeinHamster.java
@@ -1,15 +1,15 @@
 
 
 import de.hamster.debugger.model.Territorium;import de.hamster.debugger.model.Territory;import de.hamster.model.HamsterException;import de.hamster.model.HamsterInitialisierungsException;import de.hamster.model.HamsterNichtInitialisiertException;import de.hamster.model.KachelLeerException;import de.hamster.model.MauerDaException;import de.hamster.model.MaulLeerException;import de.hamster.model.MouthEmptyException;import de.hamster.model.WallInFrontException;import de.hamster.model.TileEmptyException;import de.hamster.debugger.model.Hamster;class MeinHamster extends Hamster {
-	private Spielfeld spielfeld;
+	private Karte spielfeld;
 	private int energieLevel;
     
     public MeinHamster(int p1, int p2, int p3, int p4){
     	super(p1,p2,p3,p4);
-   		spielfeld = new Spielfeld();
+   		spielfeld = new Karte();
     }
 
-	private MeinHamster dreheInRichtung(int Richtung) throws HamsterEnergieException
+	public MeinHamster dreheInRichtung(int Richtung) throws HamsterEnergieException
 	{
 		if(Richtung>3 || Richtung < 0)return this;
 		while(this.getBlickrichtung()!=Richtung){
--- Spielfeld.java
+++ Spielfeld.java
@@ -1,3 +1,15 @@
 import de.hamster.debugger.model.Territorium;import de.hamster.debugger.model.Territory;import de.hamster.model.HamsterException;import de.hamster.model.HamsterInitialisierungsException;import de.hamster.model.HamsterNichtInitialisiertException;import de.hamster.model.KachelLeerException;import de.hamster.model.MauerDaException;import de.hamster.model.MaulLeerException;import de.hamster.model.MouthEmptyException;import de.hamster.model.WallInFrontException;import de.hamster.model.TileEmptyException;import de.hamster.debugger.model.Hamster;class Spielfeld {
     
+    public int anzahlSpalten;
+    public int anzahlReihen;
+    
+    public Spielfeld(int width, int height){
+    	anzahlSpalten = width;
+    	anzahlReihen = height;
+    }
+    
+    public int getChance(int reihe, int spalte){
+    	return 0;
+    }
+    
 }
