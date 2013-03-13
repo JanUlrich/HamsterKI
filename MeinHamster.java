7b3e04faa05ac9af09be08b807c32e3946dc8b0d
JanUlrich <janulrich@anonymous.(none)>
2013-03-13 12:38:40 +0100

    Beginn eines Wegberechnungsalgos

--- MeinHamster.java
+++ MeinHamster.java
@@ -1,17 +1,19 @@
 
 
 import de.hamster.debugger.model.Territorium;import de.hamster.debugger.model.Territory;import de.hamster.model.HamsterException;import de.hamster.model.HamsterInitialisierungsException;import de.hamster.model.HamsterNichtInitialisiertException;import de.hamster.model.KachelLeerException;import de.hamster.model.MauerDaException;import de.hamster.model.MaulLeerException;import de.hamster.model.MouthEmptyException;import de.hamster.model.WallInFrontException;import de.hamster.model.TileEmptyException;import de.hamster.debugger.model.Hamster;class MeinHamster extends Hamster {
-	private Karte spielfeld;
+	private Karte karte;
 	private int energieLevel;
     
-    public MeinHamster(int p1, int p2, int p3, int p4){
-    	super(p1,p2,p3,p4);
-   		spielfeld = new Karte();
+    public MeinHamster(int p1, int p2, int p3, int p4, int p5){
+    	super(p1,p2,p3,p4,p5);
+   		karte = new Karte();
     }
 
 	public MeinHamster dreheInRichtung(int Richtung) throws HamsterEnergieException
 	{
-		if(Richtung>3 || Richtung < 0)return this;
+		//Sind die Parameter ungültig oder schaut der Hamster bereits in die gewünschte Richtung wird keine Energie verbraucht und keine Drehung ausgeführt.
+		if(Richtung>3 || Richtung < 0 || this.getBlickrichtung() == Richtung)return this;
+		//
 		while(this.getBlickrichtung()!=Richtung){
 			this.linksUm();
 		}
--- test1.java
+++ test1.java
@@ -1,5 +1,5 @@
 import de.hamster.debugger.model.Territorium;import de.hamster.debugger.model.Territory;import de.hamster.model.HamsterException;import de.hamster.model.HamsterInitialisierungsException;import de.hamster.model.HamsterNichtInitialisiertException;import de.hamster.model.KachelLeerException;import de.hamster.model.MauerDaException;import de.hamster.model.MaulLeerException;import de.hamster.model.MouthEmptyException;import de.hamster.model.WallInFrontException;import de.hamster.model.TileEmptyException;import de.hamster.debugger.model.Hamster;public class test1 implements de.hamster.model.HamsterProgram {public void main() {
-    MeinHamster hamster = new MeinHamster();
-    hamster.init(5,5,0,0,3);
+    MeinHamster hamster = new MeinHamster(5,5,0,0,3);
+
 }
 }
