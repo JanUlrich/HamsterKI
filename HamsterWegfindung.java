f5bf1649faceba72942f00f819f926d63adf07e2
Jan Bader <jan@javil.eu>
2013-03-20 10:46:25 +0100

    Get Count

--- HamsterWegfindung.java
+++ HamsterWegfindung.java
@@ -4,6 +4,7 @@
  */
 import de.hamster.debugger.model.Territorium;import de.hamster.debugger.model.Territory;import de.hamster.model.HamsterException;import de.hamster.model.HamsterInitialisierungsException;import de.hamster.model.HamsterNichtInitialisiertException;import de.hamster.model.KachelLeerException;import de.hamster.model.MauerDaException;import de.hamster.model.MaulLeerException;import de.hamster.model.MouthEmptyException;import de.hamster.model.WallInFrontException;import de.hamster.model.TileEmptyException;import de.hamster.debugger.model.Hamster;class HamsterWegfindung {
     Spielfeld spielfeld;
+    Hamster initialHamster;
     
     /**
      * @param hamster - Die Ausgangslage des Hamsters. Dabei ist nur die Position und die Richtung des Hamsters relevant.
@@ -11,18 +12,31 @@ import de.hamster.debugger.model.Territorium;import de.hamster.debugger.model.Te
      */
     public HamsterWegfindung(Hamster hamster, Spielfeld felder){
     	spielfeld = felder;
+    	initialHamster = hamster;
     }
 
-	
-	public void berechneEntfernungen(){
-		
+	public int getBestMove(){
+		berechneEntferungen();
 	}
 	
-	private void doStepfrom(int reihe, int spalte, int richtung){
+	private void berechneEntfernungen(){
+		doStepfrom(initialHamster.getPosition());
+	}
+	
+	private void doStepfrom(Position pos){
 		/*
 		Probiere alle Richtungen durch. Falls ein Feld gefunden wird,
 		auf dem der hamster noch nicht in entsprechender Richtung war oder hier ein neuer bester Energiewe
 		*/
-		
+		int momentaneEnergie = spielfeld.getEnergie(pos);
+		for(int direction = 0; direction<4;direction++){
+			int benoetigteEnergie = 2;
+			if(pos.direction == direction) benoetigteEnergie = 1; // Schaut der Hamster schon in entsprechende Richtung, braucht er sich nicht mehr drehen -> spart einen Energiepunkt
+			
+			if(spielfeld.istBessererEnergiewert(pos.getPositionInRichtung(direction), momentaneEnergie + benoetigteEnergie)){
+				spielfeld.setEnergie(pos.getPositionInRichtung(direction), momentaneEnergie + benoetigteEnergie);
+				doStepfrom(pos.getPositionInRichtung(direction)); // 
+			}
+		}
 	}
 }
--- test1.java
+++ test1.java
@@ -1,14 +1,21 @@
 import de.hamster.debugger.model.Territorium;import de.hamster.debugger.model.Territory;import de.hamster.model.HamsterException;import de.hamster.model.HamsterInitialisierungsException;import de.hamster.model.HamsterNichtInitialisiertException;import de.hamster.model.KachelLeerException;import de.hamster.model.MauerDaException;import de.hamster.model.MaulLeerException;import de.hamster.model.MouthEmptyException;import de.hamster.model.WallInFrontException;import de.hamster.model.TileEmptyException;import de.hamster.debugger.model.Hamster;public class test1 implements de.hamster.model.HamsterProgram {public void main() {
+	int gesamtAnzahlKoerner = Territorium.getAnzahlKoerner();
     MeinHamster hamster = new MeinHamster(5,5,0,0,3, 20);
     
+	mainLoop(hamster);
+	    
+	int uebrigeAnzahlKoerner = Territorium.getAnzahlKoerner();
+    
+    System.out.println("Ende");   
+}
+
+void mainLoop(MeinHamster hamster) {
     while(true) {
     	try {
 	    	hamster.kiStep();
     	} catch(HamsterEnergieException e) {
-    		break;
+    		return;
     	}
     }
-    
-    System.out.println("Ende");
 }
 }
